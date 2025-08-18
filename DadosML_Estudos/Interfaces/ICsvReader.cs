350e5bf6a3a811c15124d0078d8731edc9f11c7a
walter <walter-eidi@hotmail.com>
2025-08-18 20:24:58 -0300

    Iniciado prototipo para converter dados

---
+++ DadosML_Estudos/Interfaces/ICsvReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosML_Estudos.Interfaces
+{
+    internal interface ICsvReader
+    {
+        void ReadCsv();
+        void WriteResult();
+    }
+}
--- DadosML_Estudos/Services/CsvReader.cs
+++ DadosML_Estudos/Services/CsvReader.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
+using CsvHelper;
+using DadosML_Estudos.Interfaces;
 
 namespace DadosML_Estudos.Services
 {
-    internal class CsvReader
+    internal class CsvReader : ICsvReader
     {
+        private Stream CsvFile { get; set; }
+
+        public CsvReader(string fileName)
+        {
+        }
+        public void ReadCsv()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteResult()
+        {
+            throw new NotImplementedException();
+        }
     }
+}
+
+
+Console.WriteLine("Hello, World!");
+
+// Exemplo de leitura de CSV
+using var reader = new StreamReader("dados.csv");
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+var registros = csv.GetRecords<dynamic>();
+foreach (var registro in registros)
+{
+    Console.WriteLine(registro);
 }
